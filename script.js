f6379842493f375e56bb62624185dc13720d6541
Mark MacAller <macaller@bellsouth.net>
2018-03-29 16:37:48 -0400

    add pages

---
+++ script.js
@@ -0,0 +1,234 @@
+// Get the modal
+var modal = document.getElementsByClassName('modal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("btn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
+btn[2].onclick = function() {
+    modal[2].style.display = "block";
+}
+btn[3].onclick = function() {
+  modal[3].style.display = "block";
+  }
+btn[4].onclick = function() {
+      modal[4].style.display = "block";
+  }
+btn[5].onclick = function() {
+    modal[5].style.display = "block";
+}
+btn[6].onclick = function() {
+  modal[6].style.display = "block";
+}
+btn[7].onclick = function() {
+  modal[7].style.display = "block";
+}
+btn[8].onclick = function() {
+  modal[8].style.display = "block";
+}
+btn[9].onclick = function() {
+  modal[9].style.display = "block";
+}
+btn[10].onclick = function() {
+  modal[10].style.display = "block";
+}
+btn[11].onclick = function() {
+  modal[11].style.display = "block";
+}
+btn[12].onclick = function() {
+  modal[12].style.display = "block";
+}
+btn[13].onclick = function() {
+  modal[13].style.display = "block";
+}
+btn[14].onclick = function() {
+  modal[14].style.display = "block";
+}
+btn[15].onclick = function() {
+  modal[15].style.display = "block";
+}
+btn[16].onclick = function() {
+  modal[16].style.display = "block";
+}
+btn[17].onclick = function() {
+  modal[17].style.display = "block";
+}
+btn[18].onclick = function() {
+  modal[18].style.display = "block";
+}
+btn[19].onclick = function() {
+  modal[19].style.display = "block";
+}
+btn[20].onclick = function() {
+  modal[20].style.display = "block";
+}
+btn[21].onclick = function() {
+  modal[21].style.display = "block";
+}
+btn[22].onclick = function() {
+  modal[22].style.display = "block";
+}
+btn[23].onclick = function() {
+  modal[23].style.display = "block";
+}
+btn[24].onclick = function() {
+  modal[24].style.display = "block";
+}
+btn[25].onclick = function() {
+  modal[25].style.display = "block";
+}
+btn[26].onclick = function() {
+  modal[26].style.display = "block";
+}
+btn[27].onclick = function() {
+  modal[27].style.display = "block";
+}
+btn[28].onclick = function() {
+  modal[28].style.display = "block";
+}
+btn[29].onclick = function() {
+  modal[29].style.display = "block";
+}
+btn[30].onclick = function() {
+  modal[30].style.display = "block";
+}
+btn[31].onclick = function() {
+  modal[31].style.display = "block";
+}
+btn[32].onclick = function() {
+  modal[32].style.display = "block";
+}
+btn[33].onclick = function() {
+  modal[33].style.display = "block";
+}
+btn[34].onclick = function() {
+  modal[34].style.display = "block";
+}
+btn[35].onclick = function() {
+  modal[35].style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+
+span[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+span[3].onclick = function() {
+    modal[3].style.display = "none";
+}
+span[4].onclick = function() {
+    modal[4].style.display = "none";
+}
+span[5].onclick = function() {
+    modal[5].style.display = "none";
+}
+span[6].onclick = function() {
+    modal[6].style.display = "none";
+}
+span[7].onclick = function() {
+    modal[7].style.display = "none";
+}
+span[8].onclick = function() {
+    modal[8].style.display = "none";
+}
+span[9].onclick = function() {
+    modal[9].style.display = "none";
+}
+span[10].onclick = function() {
+    modal[10].style.display = "none";
+}
+span[11].onclick = function() {
+    modal[11].style.display = "none";
+}
+span[12].onclick = function() {
+    modal[12].style.display = "none";
+}
+span[13].onclick = function() {
+    modal[13].style.display = "none";
+}
+span[14].onclick = function() {
+    modal[14].style.display = "none";
+}
+span[15].onclick = function() {
+    modal[15].style.display = "none";
+}
+span[16].onclick = function() {
+    modal[16].style.display = "none";
+}
+span[17].onclick = function() {
+  modal[17].style.display = "none";
+}
+span[18].onclick = function() {
+  modal[18].style.display = "none";
+}
+span[19].onclick = function() {
+  modal[19].style.display = "none";
+}
+span[20].onclick = function() {
+  modal[20].style.display = "none";
+}
+span[21].onclick = function() {
+  modal[21].style.display = "none";
+}
+span[22].onclick = function() {
+  modal[22].style.display = "none";
+}
+span[23].onclick = function() {
+  modal[23].style.display = "none";
+}
+span[24].onclick = function() {
+  modal[24].style.display = "none";
+}
+span[25].onclick = function() {
+  modal[25].style.display = "none";
+}
+span[26].onclick = function() {
+  modal[26].style.display = "none";
+}
+span[27].onclick = function() {
+  modal[27].style.display = "none";
+}
+span[28].onclick = function() {
+  modal[28].style.display = "none";
+}
+span[29].onclick = function() {
+  modal[29].style.display = "none";
+}
+span[30].onclick = function() {
+  modal[30].style.display = "none";
+}
+span[31].onclick = function() {
+  modal[31].style.display = "none";
+}
+span[32].onclick = function() {
+  modal[32].style.display = "none";
+}
+span[33].onclick = function() {
+  modal[33].style.display = "none";
+}
+span[34].onclick = function() {
+  modal[34].style.display = "none";
+}
+span[35].onclick = function() {
+  modal[35].style.display = "none";
+}
+
